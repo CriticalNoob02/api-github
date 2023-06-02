85f4be78835c471622815695140126fe353fe0b6
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-06-02 17:00:40 -0300

    feat: add events datas

---
+++ dbGithub/setGithubDataEvents.js
@@ -0,0 +1,31 @@
+const { Octokit } = require("octokit")
+require('dotenv').config()
+
+const octokit = new Octokit({
+    auth: process.env.TOKEN_GITHUB
+});
+
+async function getDate(user) {
+    try {
+      const response = await octokit.request("GET /users/{username}/events", {
+        username: user,
+      })
+      
+      console.log(`Conectado! Status: ${response.status}.`)
+      
+      const data = response.data
+      const dates = []
+
+      for(let i = 0; i < data.length; i++){
+        let date = data[i].created_at
+        dates.push(date.slice(0,10))
+      } 
+      console.log(dates)
+      return dates
+    } catch (error) {
+        console.log(`Error! Status: ${error.status}.`)
+        throw error
+      }
+}
+
+module.exports = getDate
